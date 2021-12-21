ea9e9b52d04fa51a6c8fe780b5e389702c20b887
dhakal79 <n.dhakal@un-ihe.org>
2021-12-21 23:24:18 +0000

    create list of post in the home page with the detail link directed to the separate page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import HomeView
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -8,5 +8,6 @@ urlpatterns = [
     # path('members/', include('django.contrib.auth.urls')),
     # path('members/', include('members.url')),
     #path('' , views.home, name="home"),
-    path('', HomeView.as_view(), name ="home",)
+    path('', HomeView.as_view(), name ="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
