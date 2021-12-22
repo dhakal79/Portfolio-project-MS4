c2e4662423ddb515f03d271ce1b1827163c23609
dhakal79 <n.dhakal@un-ihe.org>
2021-12-22 23:34:25 +0000

    add prepopulated field in admin.py to automatically generate the slug

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content')
+
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('body')
