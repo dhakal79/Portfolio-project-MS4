04247d9c82431d40759babf818e9420e475f21ca
dhakal79 <n.dhakal@un-ihe.org>
2022-02-07 22:48:23 +0000

    add correct static files dirs and root

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', null=True, blank=True, )
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
