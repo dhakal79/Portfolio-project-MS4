a1ca4c68b22677426d3635d65bce0eb337b3408b
dhakal79 <n.dhakal@un-ihe.org>
2022-02-18 21:34:45 +0000

    add css style to the page

--- blog/admin.py
+++ blog/admin.py
@@ -3,10 +3,10 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Category, Comment 
 
 
-@admin.register(Post)
-#admin.site.register(Post)
-#admin.site.register(Category)
-#admin.site.register(Comment)
+# admin.register(Post)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
 
 class PostAdmin(SummernoteModelAdmin):
 
